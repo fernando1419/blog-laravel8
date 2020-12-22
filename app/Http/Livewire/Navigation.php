752f6a471e077e6d37f7703799a5f3f95dd3f4dc
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-12-21 23:31:05 -0300

    Added links and functionality to navigation menu items

--- app/Http/Livewire/Navigation.php
+++ app/Http/Livewire/Navigation.php
@@ -3,11 +3,14 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Category;
 
 class Navigation extends Component
 {
-    public function render()
-    {
-        return view('livewire.navigation');
-    }
+   public function render()
+   {
+      $categories = Category::all();
+
+      return view('livewire.navigation', compact('categories'));
+   }
 }
